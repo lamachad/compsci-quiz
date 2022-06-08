4bde982ae741ca7a64a39630d2379939e557734e
Leandro <machadoleandro98@gmail.com>
2022-06-08 03:33:50 -0700

    fully responsive index page

---
+++ script.js
@@ -0,0 +1,13 @@
+var messageArray = ["console.log('Ready to Start?');"];
+var textPosition = 0;
+var speed = 50;
+
+typewriter = () => {
+    document.querySelector("#message").
+    innerHTML = messageArray[0].substring(0, textPosition)
+     + "<span>\u25ae</span>";
+     if(textPosition++ != messageArray[0].length);
+     setTimeout(typewriter, speed);
+     
+}
+window.addEventListener("load", typewriter);
