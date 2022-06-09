4102e63314ef651c39a0d7c5566b94667d5caec7
Leandro <machadoleandro98@gmail.com>
2022-06-09 12:15:50 -0700

    general skeleton for quiz page

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var speed = 50;
 typewriter = () => {
     document.querySelector("#message").
     innerHTML = messageArray[0].substring(0, textPosition)
-     + "<span>\u25ae</span>";
+     + "<span class= 'bb'>\u25ae</span>";
      if(textPosition++ != messageArray[0].length);
      setTimeout(typewriter, speed);
      
