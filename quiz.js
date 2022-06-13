00688c3fa8becde93de2d3b0b0bdaf3468ffca29
Leandro <machadoleandro98@gmail.com>
2022-06-12 18:38:24 -0700

    Added question array

---
+++ quiz.js
@@ -0,0 +1,153 @@
+var quizQuestions = [
+    {
+        question: "What does CSS stand for?",
+        answers: ["Cascading Style Sheets","Color Style Source","Computer Style Source","Color Shades Sheets"],
+        answer: "Cascading Style Sheets",
+    },
+    {
+        question: "Which of the following is a programming language?",
+        answers: ["HTML","JavaScript","CSS","BootStrap"],
+        answer: "JavaScript",
+    },
+    {
+        question: "Who is the creator of JavaScript?",
+        answers: ["James Gosling","Farish Kashefinejad","Brendan Eich","John Java"],
+        answer: "Brendan Eich",
+    },
+    {
+        question: "In which type of files are DIV tags usually found in?",
+        answers: ["HTML files","JavaScript files","CSS files",""],
+        answer: "JavaScript files",
+    },
+    {
+        question: "Who is known as the father of computers?",
+        answers: ["Charles Babbage","Steve Jobs","Tim Cook","Bill Gates"],
+        answer: "Charles Babbage",
+    },
+    {
+        question: "What was the name of the first computer programmer?",
+        answers: ["Alberto Einstein","Isaac Newton","Elon Musk","Ada Lovelace"],
+        answer: "Ada Lovelace",
+    },
+    {
+        question: "What is the computer's main circuit board called",
+        answers: ["motherboard","processor","RAM board","CPU"],
+        answer: "motherboard",
+    },
+    {
+        question: "Which information storage is used to store short-term running programs and data in a computer?",
+        answers: ["Cookies","RAM","Graphics Card","Processor"],
+        answer: "RAM",
+    },
+    {
+        question: "Which part of the computer fetches, decodes, and executes the programming instructions?",
+        answers: ["CPU (Central Processing Unit)","RAM (Random Access Memory)","Graphics Card","Finder"],
+        answer: "CPU (Central Processing Unit)",
+    },
+    {
+        question: "Which two people co-founded Apple Inc. in 1977",
+        answers: ["Steve Jobs and Steve Wozniak","Steve Jobs and Tim Cook","Tim Cook and Steve Wozniak","Steve Wozniak and Bill Gates"],
+        answer: "Steve Jobs and Steve Wozniak",
+    },
+    {
+        question: "What is the name of the first operating system designed by Microsoft?",
+        answers: ["MS-DOS","IOS","Mac OS","Watch OS"],
+        answer: "MS-DOS",
+    },
+    {
+        question: "Who is currently the CEO of Microsoft?",
+        answers: ["Tim Cook","Bill Gates","None of the options","Satya Nadella"],
+        answer: "Satya Nadella",
+    },
+    {
+        question: "What is the name of Apple's desktop operating system?",
+        answers: ["VOS","IOS","Mac OS","DeskOS"],
+        answer: "Mac OS",
+    },
+    {
+        question: "Which company invented the USB port?",
+        answers: ["Intel","Apple ","Tesla","Google"],
+        answer: "Intel",
+    },
+    {
+        question: "'Firefox', 'Chrome', and 'Safari' are what type of computer software?",
+        answers: ["Web Browsers","Internet Providers","Serfers","none"],
+        answer: "Web Browsers",
+    },
+    {
+        question: "What application programs are used to organize, store, and modify data in computers?",
+        answers: ["Database Management Systems","Local Storage","Cloud Storage","USB Drive"],
+        answer: "Database Management Systems",
+    },
+    {
+        question: "How many heading tags are in HTML",
+        answers: ["1","6","7","3"],
+        answer: "6",
+    },
+    {
+        question: "Which hexadecimal combination displays the color red?",
+        answers: ["#FF0000","#00FF00"," #0000FF","#FFFFFF"],
+        answer: "#FF0000",
+    },
+    {
+        question: "Which HTML attribute points to images, audio files, and video clips?",
+        answers: ["src","id","href","class"],
+        answer: "scr",
+    },
+    {
+        question: "The SCRIPT tag enables you to enhance a web page with code written in which programming language?",
+        answers: ["JavaScript","SQL","Pascal","FORTRAN"],
+        answer: "JavaScript",
+    },
+    {
+        question: "How many bytes are in 1 Kilobyte?",
+        answers: ["1000","100","0.0001","10"],
+        answer: "1000",
+    },
+    {
+        question: "What is a computer program that translates computer code from one programming language into another called?",
+        answers: ["transcoder","compiler","reader","interpreter"],
+        answer: "compiler",
+    },
+    {
+        question: "True or false statements are represented by?",
+        answers: ["Booleans","Classes","Numbers","Identifiers"],
+        answer: "Booleans",
+    },
+    {
+        question: "1000 kilobytes is equal to 1 what?",
+        answers: ["Megabyte","Gigabyte","Byte","Terabyte"],
+        answer: "Megabyte",
+    },
+    {
+        question: "What is an error, flaw or fault in a computer program or system called?",
+        answers: ["A bug","A cookie","A cache","A virus"],
+        answer: "A bug",
+    },
+    {
+        question: "What are very low-level languages called?",
+        answers: ["programming languages","compilers","assembly languages","binary"],
+        answer: "assembly languages",
+    },
+    {
+        question: "1000 megabytes is equal to 1 what?",
+        answers: ["Gigabyte","Megabyte","Terabyte","Kilobyte"],
+        answer: "Gigabyte",
+    },
+    {
+        question: "What is the process of starting a computer called?",
+        answers: ["booting","restarting","powering down","turning on"],
+        answer: "booting",
+    },
+    {
+        question: "What does 'HDD' stand for in regards to computer storage?",
+        answers: ["Hard Drive Disk","Hot Disk Directory","Helper Directory Disk","Hard Drive Directory"],
+        answer: "Hard Drive Disk",
+    },
+    {
+        question: "What language did Apple develope to replace Objective-C?",
+        answers: ["Ruby","Swift","React Native","Kotlin"],
+        answer: "Swift",
+    },
+    
+]
